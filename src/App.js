c213e2404e581b36e6ef44577fd9c8be438d1c58
Seckin Ozturk <franseck102@gmail.com>
2024-09-21 21:42:56 -0400

    context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import AppRouter from './router/AppRouter'
+import AuthProvider from './context/AuthProvider'
 
 const App = () => {
   return (
-    <div>
+   <AuthProvider>
       <AppRouter/>
-    </div>
+</AuthProvider>
   )
 }
 
