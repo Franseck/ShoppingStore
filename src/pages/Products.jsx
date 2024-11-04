d112188df2ed51fef88b823f97b2dfdd6163e0fc
Seckin Ozturk <franseck102@gmail.com>
2024-11-03 23:47:22 -0500

    product

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,17 @@
 import React, { useState } from 'react'
+import axios from 'axios';
 
 const Products = () => {
 
   const [product, setProduct] = useState([]);
+  const [search, setSearch] = useState("");
+
+  try {
+    
+  } catch (error) {
+    
+  }
+
   return (
     <div>Products</div>
   )
